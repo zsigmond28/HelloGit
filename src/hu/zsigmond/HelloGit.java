0f9627d22d192827fcfc9036c3507eaba9dc51ab
Attila Zsigmond <email@email.com>
2020-03-18 13:05:12 +0100

    Masodik println

--- src/hu/zsigmond/HelloGit.java
+++ src/hu/zsigmond/HelloGit.java
@@ -3,5 +3,6 @@ package hu.zsigmond;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Commit-álok szépen");
     }
 }
